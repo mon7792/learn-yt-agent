8e882e89c51c18a27a984a6a3073cc08841be758
mon7792 <monish7792@gmail.com>
2025-02-27 10:53:16 +0000

    feat: added schematic

--- provider/client/index.tsx
+++ provider/client/index.tsx
@@ -1,15 +1,20 @@
 "use client";
 import { ThemeProvider } from "@/provider/theme";
-
+import { SchematicProvider } from "@schematichq/schematic-react";
 export default function ClientWrapper({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  if (!process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY) {
+    throw new Error("NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY is not set");
+  }
+
   return (
-    
-      <ThemeProvider>
-        {children}
-      </ThemeProvider>
+    <SchematicProvider
+      publishableKey={process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY}
+    >
+      <ThemeProvider>{children}</ThemeProvider>
+    </SchematicProvider>
   );
 }
